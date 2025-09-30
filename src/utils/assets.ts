fa56f1228b2de227729d5e39acc53225e8648be7
Carlos Ferreras <carlos@carlosferreras.com>
2025-09-30 18:09:28 +0200

    Refactor asset imports and centralized asset management
    - Moved champion and item asset imports to a centralized `assets.ts` file for better organization and maintainability.
    - Updated `ItemWithImage.astro` to utilize the new `getItemImage` function from `assets.ts`.
    - Simplified `TFTBoardCharacter.astro` by removing individual champion and item imports, now using centralized assets.
    - Modified `index.astro` to use `getChampionImage` from the new centralized assets.
    - Added helper functions in `assets.ts` for retrieving champion and item images, checking existence, and listing available names.

---
+++ src/utils/assets.ts
@@ -0,0 +1,316 @@
+// ============================================================================
+// CHAMPIONS ASSETS - Importaciones centralizadas
+// ============================================================================
+
+// Importar todos los assets de campeones
+import AatroxImg from "../assets/Champions/Aatrox.png";
+import AhriImg from "../assets/Champions/Ahri.png";
+import AkaliImg from "../assets/Champions/Akali.png";
+import AsheImg from "../assets/Champions/Ashe.png";
+import BraumImg from "../assets/Champions/Braum.png";
+import CaitlynImg from "../assets/Champions/Caitlyn.png";
+import DariusImg from "../assets/Champions/Darius.png";
+import DrMundoImg from "../assets/Champions/Dr Mundo.png";
+import EzrealImg from "../assets/Champions/Ezreal.png";
+import GangplankImg from "../assets/Champions/Gangplank.png";
+import GarenImg from "../assets/Champions/Garen.png";
+import GnarImg from "../assets/Champions/Gnar.png";
+import GwenImg from "../assets/Champions/Gwen.png";
+import JannaImg from "../assets/Champions/Janna.png";
+import JarvanIVImg from "../assets/Champions/JarvanIV.png";
+import JayceImg from "../assets/Champions/Jayce.png";
+import JhinImg from "../assets/Champions/Jhin.png";
+import JinxImg from "../assets/Champions/Jinx.png";
+import KaiSaImg from "../assets/Champions/KaiSa.png";
+import KalistaImg from "../assets/Champions/Kalista.png";
+import KarmaImg from "../assets/Champions/Karma.png";
+import KatarinaImg from "../assets/Champions/Katarina.png";
+import KayleImg from "../assets/Champions/Kayle.png";
+import KennenImg from "../assets/Champions/Kennen.png";
+import KobukoImg from "../assets/Champions/Kobuko.png";
+import KogMawImg from "../assets/Champions/KogMaw.png";
+import KSanteImg from "../assets/Champions/KSante.png";
+import LeeSinImg from "../assets/Champions/Lee Sin.png";
+import LeonaImg from "../assets/Champions/Leona.png";
+import LucianImg from "../assets/Champions/Lucian.png";
+import LuluImg from "../assets/Champions/Lulu.png";
+import LuxImg from "../assets/Champions/Lux.png";
+import MalphiteImg from "../assets/Champions/Malphite.png";
+import MalzaharImg from "../assets/Champions/Malzahar.png";
+import NaafiriImg from "../assets/Champions/Naafiri.png";
+import NeekoImg from "../assets/Champions/Neeko.png";
+import PoppyImg from "../assets/Champions/Poppy.png";
+import RakanImg from "../assets/Champions/Rakan.png";
+import RammusImg from "../assets/Champions/Rammus.png";
+import RellImg from "../assets/Champions/Rell.png";
+import RyzeImg from "../assets/Champions/Ryze.png";
+import SamiraImg from "../assets/Champions/Samira.png";
+import SennaImg from "../assets/Champions/Senna.png";
+import SeraphineImg from "../assets/Champions/Seraphine.png";
+import SettImg from "../assets/Champions/Sett.png";
+import ShenImg from "../assets/Champions/Shen.png";
+import SivirImg from "../assets/Champions/Sivir.png";
+import SmolderImg from "../assets/Champions/Smolder.png";
+import SwainImg from "../assets/Champions/Swain.png";
+import SyndraImg from "../assets/Champions/Syndra.png";
+import TheMightyMechImg from "../assets/Champions/The Mighty Mech.png";
+import TwistedFateImg from "../assets/Champions/Twisted Fate.png";
+import UdyrImg from "../assets/Champions/Udyr.png";
+import VarusImg from "../assets/Champions/Varus.png";
+import ViImg from "../assets/Champions/Vi.png";
+import ViegoImg from "../assets/Champions/Viego.png";
+import VolibearImg from "../assets/Champions/Volibear.png";
+import XayahImg from "../assets/Champions/Xayah.png";
+import XinZhaoImg from "../assets/Champions/Xin Zhao.png";
+import YasuoImg from "../assets/Champions/Yasuo.png";
+import YoneImg from "../assets/Champions/Yone.png";
+import YuumiImg from "../assets/Champions/Yuumi.png";
+import ZacImg from "../assets/Champions/Zac.png";
+import ZiggsImg from "../assets/Champions/Ziggs.png";
+import ZyraImg from "../assets/Champions/Zyra.png";
+
+// ============================================================================
+// ITEMS ASSETS - Importaciones centralizadas
+// ============================================================================
+
+// Items básicos
+import BfSwordImg from "../assets/items/bf-sword.png";
+import ChainVestImg from "../assets/items/chain-vest.png";
+import FryingPanImg from "../assets/items/frying_pan.png";
+import GiantsBeltImg from "../assets/items/giants-belt.png";
+import NeedlesslyLargeRodImg from "../assets/items/needlessly-large-rod.png";
+import NegatronCloakImg from "../assets/items/negatron-cloak.png";
+import RecurveBowImg from "../assets/items/recurve-bow.png";
+import SparringGlovesImg from "../assets/items/sparring-gloves.png";
+import SpatulaImg from "../assets/items/Spatula.png";
+import TearOfTheGoddessImg from "../assets/items/tear-of-the-goddess.png";
+
+// Items combinados (selección principal)
+import AdaptiveHelmImg from "../assets/items/combined/adaptive-helm.png";
+import ArchangelsStaffImg from "../assets/items/combined/archangels-staff.png";
+import BloodthirsterImg from "../assets/items/combined/bloodthirster.png";
+import BlueBuffImg from "../assets/items/combined/blue-buff2.png";
+import BrambleVestImg from "../assets/items/combined/bramble-vest.png";
+import CrownguardImg from "../assets/items/combined/crownguard.png";
+import DeathbladeImg from "../assets/items/combined/deathblade.png";
+import DragonClawImg from "../assets/items/combined/dragons-claw.png";
+import EdgeOfNightImg from "../assets/items/combined/edge-of-night.png";
+import EvenshroudImg from "../assets/items/combined/evenshroud.png";
+import FimbulwinterImg from "../assets/items/combined/fimbulwinter.png";
+import GargoyleStoneplateImg from "../assets/items/combined/gargoyle-stoneplate.png";
+import GiantSlayerImg from "../assets/items/combined/giant-slayer.png";
+import GuardbreakerImg from "../assets/items/combined/guardbreaker.png";
+import GuinsoosRagebladeImg from "../assets/items/combined/guinsoos-rageblade.png";
+import HandOfJusticeImg from "../assets/items/combined/hand-of-justice.png";
+import HextechGunbladeImg from "../assets/items/combined/hextech-gunblade.png";
+import InfinityEdgeImg from "../assets/items/combined/infinity-edge.png";
+import IonicSparkImg from "../assets/items/combined/ionic-spark.png";
+import JeweledGauntletImg from "../assets/items/combined/jeweled-gauntlet.png";
+import KrakensFuryImg from "../assets/items/combined/krakensfury.png";
+import LastWhisperImg from "../assets/items/combined/last-whisper.png";
+import MorellonomiconImg from "../assets/items/combined/morellonomicon.png";
+import NashorsToothImg from "../assets/items/combined/nashors-tooth.png";
+import QuicksilverImg from "../assets/items/combined/quicksilver.png";
+import RabadonsDeathcapImg from "../assets/items/combined/rabadons-deathcap.png";
+import RedBuffImg from "../assets/items/combined/redbuff.png";
+import SpearOfShojinImg from "../assets/items/combined/spear-of-shojin2.png";
+import SpiritVisageImg from "../assets/items/combined/spiritvisage.png";
+import SteadfastHammerImg from "../assets/items/combined/steadfast-hammer.png";
+import SteraksGageImg from "../assets/items/combined/steraks-gage.png";
+import SunfireCapeImg from "../assets/items/combined/sunfire-cape.png";
+import TacticiansCrownImg from "../assets/items/combined/tacticians-crown.png";
+import TacticiansCapeImg from "../assets/items/combined/tacticians_cape.png";
+import TacticiansShieldImg from "../assets/items/combined/tacticians_shield.png";
+import ThiefsGlovesImg from "../assets/items/combined/thiefs-gloves.png";
+import TitansResolveImg from "../assets/items/combined/titans-resolve.png";
+import VoidStaffImg from "../assets/items/combined/voidstaff.png";
+import WarmogsArmorImg from "../assets/items/combined/warmogs-armor.png";
+
+// ============================================================================
+// OBJETOS DE EXPORTACIÓN
+// ============================================================================
+
+// Objeto con todos los campeones para fácil acceso
+export const championAssets = {
+  Aatrox: AatroxImg,
+  Ahri: AhriImg,
+  Akali: AkaliImg,
+  Ashe: AsheImg,
+  Braum: BraumImg,
+  Caitlyn: CaitlynImg,
+  Darius: DariusImg,
+  "Dr Mundo": DrMundoImg,
+  Ezreal: EzrealImg,
+  Gangplank: GangplankImg,
+  Garen: GarenImg,
+  Gnar: GnarImg,
+  Gwen: GwenImg,
+  Janna: JannaImg,
+  JarvanIV: JarvanIVImg,
+  Jayce: JayceImg,
+  Jhin: JhinImg,
+  Jinx: JinxImg,
+  KaiSa: KaiSaImg,
+  Kalista: KalistaImg,
+  Karma: KarmaImg,
+  Katarina: KatarinaImg,
+  Kayle: KayleImg,
+  Kennen: KennenImg,
+  Kobuko: KobukoImg,
+  KogMaw: KogMawImg,
+  KSante: KSanteImg,
+  "Lee Sin": LeeSinImg,
+  Leona: LeonaImg,
+  Lucian: LucianImg,
+  Lulu: LuluImg,
+  Lux: LuxImg,
+  Malphite: MalphiteImg,
+  Malzahar: MalzaharImg,
+  Naafiri: NaafiriImg,
+  Neeko: NeekoImg,
+  Poppy: PoppyImg,
+  Rakan: RakanImg,
+  Rammus: RammusImg,
+  Rell: RellImg,
+  Ryze: RyzeImg,
+  Samira: SamiraImg,
+  Senna: SennaImg,
+  Seraphine: SeraphineImg,
+  Sett: SettImg,
+  Shen: ShenImg,
+  Sivir: SivirImg,
+  Smolder: SmolderImg,
+  Swain: SwainImg,
+  Syndra: SyndraImg,
+  "The Mighty Mech": TheMightyMechImg,
+  "Twisted Fate": TwistedFateImg,
+  Udyr: UdyrImg,
+  Varus: VarusImg,
+  Vi: ViImg,
+  Viego: ViegoImg,
+  Volibear: VolibearImg,
+  Xayah: XayahImg,
+  "Xin Zhao": XinZhaoImg,
+  Yasuo: YasuoImg,
+  Yone: YoneImg,
+  Yuumi: YuumiImg,
+  Zac: ZacImg,
+  Ziggs: ZiggsImg,
+  Zyra: ZyraImg,
+};
+
+// Objeto con todos los items para fácil acceso
+export const itemAssets = {
+  // Items básicos
+  "BF Sword": BfSwordImg,
+  "Chain Vest": ChainVestImg,
+  "Frying Pan": FryingPanImg,
+  "Giant's Belt": GiantsBeltImg,
+  "Needlessly Large Rod": NeedlesslyLargeRodImg,
+  "Negatron Cloak": NegatronCloakImg,
+  "Recurve Bow": RecurveBowImg,
+  "Sparring Gloves": SparringGlovesImg,
+  "Spatula": SpatulaImg,
+  "Tear of the Goddess": TearOfTheGoddessImg,
+
+  // Items combinados
+  "Adaptive Helm": AdaptiveHelmImg,
+  "Archangel's Staff": ArchangelsStaffImg,
+  "Bloodthirster": BloodthirsterImg,
+  "Blue Buff": BlueBuffImg,
+  "Bramble Vest": BrambleVestImg,
+  "Crownguard": CrownguardImg,
+  "Deathblade": DeathbladeImg,
+  "Dragon's Claw": DragonClawImg,
+  "Edge of Night": EdgeOfNightImg,
+  "Evenshroud": EvenshroudImg,
+  "Fimbulwinter": FimbulwinterImg,
+  "Gargoyle Stoneplate": GargoyleStoneplateImg,
+  "Giant Slayer": GiantSlayerImg,
+  "Guardbreaker": GuardbreakerImg,
+  "Guinsoo's Rageblade": GuinsoosRagebladeImg,
+  "Hand of Justice": HandOfJusticeImg,
+  "Hextech Gunblade": HextechGunbladeImg,
+  "Infinity Edge": InfinityEdgeImg,
+  "Ionic Spark": IonicSparkImg,
+  "Jeweled Gauntlet": JeweledGauntletImg,
+  "Kraken's Fury": KrakensFuryImg,
+  "Last Whisper": LastWhisperImg,
+  "Morellonomicon": MorellonomiconImg,
+  "Nashor's Tooth": NashorsToothImg,
+  "Quicksilver": QuicksilverImg,
+  "Rabadon's Deathcap": RabadonsDeathcapImg,
+  "Red Buff": RedBuffImg,
+  "Spear of Shojin": SpearOfShojinImg,
+  "Spirit Visage": SpiritVisageImg,
+  "Steadfast Hammer": SteadfastHammerImg,
+  "Sterak's Gage": SteraksGageImg,
+  "Sunfire Cape": SunfireCapeImg,
+  "Tactician's Crown": TacticiansCrownImg,
+  "Tactician's Cape": TacticiansCapeImg,
+  "Tactician's Shield": TacticiansShieldImg,
+  "Thief's Gloves": ThiefsGlovesImg,
+  "Titan's Resolve": TitansResolveImg,
+  "Void Staff": VoidStaffImg,
+  "Warmog's Armor": WarmogsArmorImg,
+};
+
+// ============================================================================
+// FUNCIONES HELPER
+// ============================================================================
+
+/**
+ * Obtiene la imagen de un campeón a partir de una ruta de archivo o nombre
+ * @param imagePath - Ruta como "/src/assets/Champions/Braum.png" o nombre como "Braum"
+ * @returns La URL de la imagen o una imagen por defecto
+ */
+export function getChampionImage(imagePath: string): string {
+  // Si es una ruta completa, extraer el nombre del campeón
+  const match = imagePath.match(/\/([^\/]+)\.png$/);
+  const championName = match ? match[1] : imagePath;
+  
+  return championAssets[championName as keyof typeof championAssets]?.src || imagePath;
+}
+
+/**
+ * Obtiene la imagen de un item a partir del nombre
+ * @param itemName - Nombre del item como "Infinity Edge"
+ * @returns La URL de la imagen o una imagen por defecto
+ */
+export function getItemImage(itemName: string): string {
+  return itemAssets[itemName as keyof typeof itemAssets]?.src || "";
+}
+
+/**
+ * Verifica si un campeón existe en los assets
+ * @param championName - Nombre del campeón
+ * @returns True si el campeón existe
+ */
+export function hasChampion(championName: string): boolean {
+  return championName in championAssets;
+}
+
+/**
+ * Verifica si un item existe en los assets
+ * @param itemName - Nombre del item
+ * @returns True si el item existe
+ */
+export function hasItem(itemName: string): boolean {
+  return itemName in itemAssets;
+}
+
+/**
+ * Obtiene todos los nombres de campeones disponibles
+ * @returns Array con todos los nombres de campeones
+ */
+export function getChampionNames(): string[] {
+  return Object.keys(championAssets);
+}
+
+/**
+ * Obtiene todos los nombres de items disponibles
+ * @returns Array con todos los nombres de items
+ */
+export function getItemNames(): string[] {
+  return Object.keys(itemAssets);
+}
